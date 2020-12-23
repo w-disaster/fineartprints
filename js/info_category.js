e540c94056ba8e472aae789855ad6cfa9243347e
w-disaster <luca.fabri1999@gmail.com>
2020-12-23 20:15:12 +0100

    fixed hover on <a>

--- js/info_category.js
+++ js/info_category.js
@@ -1,11 +1,12 @@
 $(document).ready(function(){
+    const middle = $(this).parent().parent().children(".middle");
+
     $("img").hover(function(){
         $(this).addClass('transition');
 
-        $(this).parent().parent().children(".middle").fadeTo(0, 1);
+        middle.fadeTo(0, 1);
 
     }, function(){
-        const middle = $(this).parent().parent().children(".middle");
         middle.fadeTo(0, 0);
         if(!middle.is(":hover")){
             $(".transition").removeClass('transition');
@@ -14,6 +15,8 @@ $(document).ready(function(){
 
     $(".middle").hover(function(){
         $(this).fadeTo(0, 1);
+    }, function(){
+        $(this).fadeTo(0, 0);
     });
 
 });
