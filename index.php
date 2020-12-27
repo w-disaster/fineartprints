a89565f194d7a283f3a9aa9baa4416aee4f93c39
Riccardo Battistini <riccardo.battistini@protonmail.com>
2020-12-27 11:02:25 +0100

    Update template, categories and orders

--- index.php
+++ index.php
@@ -3,7 +3,7 @@ require_once 'bootstrap.php';
 
 //Base Template
 $templateParams["titolo"] = "Shop";
-$templateParams["nome"] = "shop.html";
+$templateParams["nome"] = "categories.php";
 
 require 'template/base.php';
 ?>
