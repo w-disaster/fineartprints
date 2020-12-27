ceb1bbd00235aff90a866efb7752f99342132732
Davide Rossi <davide.rossi39@studio.unibo.it>
2020-12-27 16:53:50 +0100

    done javascript customer form

---
+++ js/customer-form.js
@@ -0,0 +1,24 @@
+$(document).ready(function(){
+    
+    $("#edit").click(function() {
+        $("#edit").hide();
+        $("#save").removeClass("d-none");
+        $("#cancel").removeClass("d-none");
+        $("#form-old-password").removeClass("d-none");
+        $("#form-new-password").removeClass("d-none");
+        $("#form-confirm-password").removeClass("d-none");
+   }); 
+
+   $("#cancel").click(function() {
+        location.reload();
+    }); 
+
+    $("personal-form").submit(function() {
+        $("#edit").show();
+        $("#save").addClass("d-none");
+        $("#cancel").addClass("d-none");
+        $("#form-old-password").addClass("d-none");
+        $("#form-new-password").addClass("d-none");
+        $("#form-confirm-password").addClass("d-none");
+    }); 
+});
