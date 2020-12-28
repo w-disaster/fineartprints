6d8e6bbbb8ec83d49d4ba44667fedf037ec0aac9
Riccardo Battistini <riccardo.battistini@protonmail.com>
2020-12-28 11:38:44 +0100

    Add seller area, shopping cart, product and sign-in pages in php

--- js/toggle-sidebar.js
+++
@@ -1,9 +0,0 @@
-$(document).ready(function () {
-  $("button.sidebar-opener").click(function () {
-    $("div.sidebar").css("display", "block");
-  });
-
-  $("button.sidebar-closer").click(function () {
-    $("div.sidebar").css("display", "none");
-  });
-});
