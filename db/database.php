0a75e8c251f4004157c6e33ab6f8b675734bc4b8
Davide Rossi <davide.rossi39@studio.unibo.it>
2020-12-29 20:49:22 +0100

    Added homepage and sign up query in database.php and added js file in base.php

--- db/database.php
+++ db/database.php
@@ -43,5 +43,51 @@ class DatabaseHelper{
 
         return $result->fetch_all(MYSQLI_ASSOC);
     }
+
+    
+    
+    public function getSlideShowPictures($n=6){
+        $stmt = $this->db->prepare("SELECT Image FROM picture WHERE Orientation='landscape'
+         ORDER BY RAND() LIMIT ?");
+        $stmt->bind_param("i",$n);
+        $stmt->execute();
+        $result = $stmt->get_result();
+
+        return $result->fetch_all(MYSQLI_ASSOC);
+    }
+
+    public function getLatestPictures($n=4){
+        $stmt = $this->db->prepare("SELECT Image, Title FROM picture ORDER BY Publish_Date DESC LIMIT ?");
+        $stmt->bind_param("i",$n);
+        $stmt->execute();
+        $result = $stmt->get_result();
+
+        return $result->fetch_all(MYSQLI_ASSOC);
+    }
+
+    public function getRandSalesPictures($n=-1){
+        $query = "SELECT Image, Title FROM picture WHERE Discount!=0 ORDER BY RAND()";
+        if($n > 0){
+            $query = $query . " LIMIT ?";
+        }
+        $stmt = $this->db->prepare($query);
+        if($n > 0){
+            $stmt->bind_param("i", $n);
+        }
+        $stmt->execute();
+        $result = $stmt->get_result();
+
+        return $result->fetch_all(MYSQLI_ASSOC);
+    }
+
+    public function insertNewCustomer($email, $birth_date, $password, $name, $surname, $phone, $city, 
+    $postal_code, $province, $address){
+        $customer = "customer";
+        $stmt = $this->db->prepare("INSERT INTO user (Email, Birth_date, Password, Name, Surname, Phone, City, Postal_Code,
+         Province, Address, Role) VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?);");
+        $stmt->bind_param("sssssisisss", $email, $birth_date, $password, $name, $surname, $phone, $city, 
+        $postal_code, $province, $address, $customer);
+        $stmt->execute();
+    }
 }
 ?>
