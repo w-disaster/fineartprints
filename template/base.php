2c689029839750f170ec935f96f33c369431a2cf
Riccardo Battistini <riccardo.battistini@protonmail.com>
2020-12-30 15:10:31 +0100

    Update base.php with bootstrap 4.5

--- template/base.php
+++ template/base.php
@@ -5,7 +5,8 @@
   <meta charset="utf-8">
   <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
   <title>Template</title>
-  <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
+  <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.5.3/dist/css/bootstrap.min.css"
+    integrity="sha384-TX8t27EcRE3e/ihU7zmQxVncDAy5uIKz4rEkgIXeMed4M0jlfIDPvg6uqKI2xXr2" crossorigin="anonymous">
   <link rel="shortcut icon" type="image/jpg" href="upload/icons/file-richtext.svg" />
   <link rel="stylesheet" href="css/theme.css">
   <link rel="stylesheet" href="css/shop_style.css">
