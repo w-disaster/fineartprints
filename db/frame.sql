838a53124d33dd22a6de64ac3b0cb927e1a51499
Riccardo Battistini <riccardo.battistini@protonmail.com>
2021-01-05 11:37:35 +0100

    Update seller area, prints visualization

---
+++ db/frame.sql
@@ -0,0 +1,18 @@
+INSERT INTO Make_passpartout_available(Email, Passpartout_id) VALUES
+("cippa.pino@prints.com", 8001),
+("cippa.pino@prints.com", 8002),
+("cippa.pino@prints.com", 8003),
+("cippa.pino@prints.com", 8004),
+("cippa.pino@prints.com", 8005),
+("cippa.pino@prints.com", 8006);
+
+INSERT INTO Make_frame_available(Email, Frame_id) VALUES
+("cippa.pino@prints.com", 7820),
+("cippa.pino@prints.com", 7821),
+("cippa.pino@prints.com", 7822),
+("cippa.pino@prints.com", 7823),
+("cippa.pino@prints.com", 7824),
+("cippa.pino@prints.com", 7825),
+("cippa.pino@prints.com", 7826),
+("cippa.pino@prints.com", 7827),
+("cippa.pino@prints.com", 7828);
--- seller-prints.php
+++ seller-prints.php
@@ -1,8 +1,22 @@
 <?php
 require_once 'bootstrap.php';
 
-$templateParams["title"] = "Seller Area - Your prints - Fine Art Prints";
-$templateParams["name"] = "seller-prints-template.php";
+if(isUserLoggedIn(UserType::Seller)) {
+    $templateParams["title"] = "Seller Area - Your prints - Fine Art Prints";
+    $templateParams["name"] = "seller-prints-template.php";
+
+    $email = htmlspecialchars($_SESSION["Email"]);
+
+    $templateParams["prints"] = $dbh->getPicturesFromSeller($email);
+    
+
+} else {
+    $templateParams["title"] = "Page not found";
+    $templateParams["name"] = "template-404.php";
+    require "error-404.php";
+    header('HTTP/1.0 404 Not Found');
+    $_GET['e'] = 404;
+}
 
 require 'template/base.php';
 ?>
